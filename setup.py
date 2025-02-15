8e456b5ee030dfa33a90a11ae2411df16c2d5b51
geisserml <geisserml@gmail.com>
2025-02-16 00:55:59 +0100

    Bump copyright year, rm deprecated .reuse/dep5

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2022 geisserml <geisserml@gmail.com>
+# SPDX-FileCopyrightText: 2025 geisserml <geisserml@gmail.com>
 # SPDX-License-Identifier: MPL-2.0
 
 # NOTE This file may only be called by the package manager - end users should not run it directly
